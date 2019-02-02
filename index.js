b8dc8b468e7e18afca77f162b780ea72a4d77765
fxp <F(xplAur122^Github>
2019-02-02 23:43:44 +0800

    refine

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
-// Example express application adding the parse-server module to expose Parse
-// compatible API routes.
+const express = require('express');
+const ParseServer = require('parse-server').ParseServer;
+const GridStoreAdapter = require('parse-server/lib/Adapters/Files/GridStoreAdapter').GridStoreAdapter
 
-var express = require('express');
-var ParseServer = require('parse-server').ParseServer;
-var path = require('path');
+const path = require('path');
 
-var databaseUri = process.env.PARSE_SERVER_DATABASE_URI;
+const databaseUri = process.env.PARSE_SERVER_DATABASE_URI;
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
-var GridStoreAdapter = require('parse-server/lib/Adapters/Files/GridStoreAdapter').GridStoreAdapter
-
-var api = new ParseServer({
+const api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.PARSE_SERVER_CLOUD || __dirname + '/cloud/main.js',
   appId: process.env.PARSE_SERVER_APPLICATION_ID || 'myAppId',
-  masterKey: process.env.PARSE_SERVER_MASTER_KEY || '', //Add your master key here. Keep it secret!
+  masterKey: process.env.PARSE_SERVER_MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.PARSE_PUBLIC_SERVER_URL || 'http://localhost:1337/api/1',  // Don't forget to change to https if needed
   maxUploadSize: process.env.PARSE_SERVER_MAX_UPLOAD_SIZE,
-  // cacheMaxSize: process.env.PARSE_SERVER_CACHE_MAX_SIZE,
+  cacheMaxSize: parseInt(process.env.PARSE_SERVER_CACHE_MAX_SIZE),
   publicServerURL: process.env.PARSE_PUBLIC_SERVER_URL || 'http://localhost:1337/api/1',
-
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-
-  filesAdapter: new GridStoreAdapter(process.env.PARSE_SERVER_DATABASE_URI)
-
+  filesAdapter: new GridStoreAdapter(process.env.PARSE_SERVER_DATABASE_URI) // For default setting. GridFS
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
 
-var app = express();
+const app = express();
 
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
 // Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_SERVER_MOUNT_PATH || '/parse';
+const mountPath = process.env.PARSE_SERVER_MOUNT_PATH || '/parse';
 app.use(mountPath, api);
 
 // Parse Server plays nicely with the rest of your web routes
@@ -54,8 +45,8 @@ app.get('/test', function (req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-var port = process.env.PORT || 1337;
-var httpServer = require('http').createServer(app);
+const port = process.env.PORT || 1337;
+const httpServer = require('http').createServer(app);
 httpServer.listen(port, function () {
   console.log('parse-server-example running on port ' + port + '.');
 });
